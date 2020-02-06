03dd6acfae433fff770aa036dde0dcddf1baa056
adammmertel <mertel.adam@gmail.com>
2020-02-06 13:56:03 +0100

    DEMO: update

--- demo/main.js
+++ demo/main.js
@@ -1,68 +1,66 @@
 var markerGroup;
 var map;
 
-document.addEventListener("DOMContentLoaded", function() {
-  map = L.map("map-content").setView([37, 32], 6);
+document.addEventListener('DOMContentLoaded', function() {
+	map = L.map('map-content').setView([ 37, 32 ], 6);
 
-  L.tileLayer(
-    "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
-    {
-      attribution: "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ",
-      maxZoom: 16
-    }
-  ).addTo(map);
+	L.tileLayer(
+		'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+		{
+			attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+			maxZoom: 16
+		}
+	).addTo(map);
 
-  render();
+	render();
 
-  var selects = document.getElementsByTagName("select");
-  for (var si in selects) {
-    var select = selects[si];
+	var selects = document.getElementsByTagName('select');
+	for (var si in selects) {
+		var select = selects[si];
 
-    if (select.addEventListener) {
-      select.addEventListener("change", function() {
-        render();
-      });
-    }
-  }
+		if (select.addEventListener) {
+			select.addEventListener('change', function() {
+				render();
+			});
+		}
+	}
 });
 
 var getOptions = function() {
-  return {
-    maxDist: parseInt(document.getElementById("select-max-distance").value),
-    noSteps: parseInt(document.getElementById("select-no-steps").value),
-    circleSegmentAngle: parseInt(document.getElementById("select-angle").value),
-    opacityDecrease: parseFloat(
-      document.getElementById("select-opacity-decrease").value
-    ),
-    maxOpacity: parseFloat(document.getElementById("select-max-opacity").value),
-    colors: {
-      Sarapis: "#ff7f00",
-      Isis: "#377eb8",
-      Apis: "#4daf4a",
-      Anubis: "#e41a1c"
-    },
-    propertyName: "deities"
-  };
+	return {
+		maxDist: parseInt(document.getElementById('select-max-distance').value),
+		noSteps: parseInt(document.getElementById('select-no-steps').value),
+		circleSegmentAngle: parseInt(document.getElementById('select-angle').value),
+		opacityDecrease: parseFloat(document.getElementById('select-opacity-decrease').value),
+		maxOpacity: parseFloat(document.getElementById('select-max-opacity').value),
+		colors: {
+			Sarapis: '#ff7f00',
+			Isis: '#377eb8',
+			Apis: '#4daf4a',
+			Anubis: '#e41a1c'
+		},
+		propertyName: 'deities'
+	};
 };
 
 var render = function() {
-  console.log("render");
-  var options = getOptions();
+	console.log('render');
+	var options = getOptions();
 
-  console.log(options);
+	console.log(options);
 
-  if (map.hasLayer(markerGroup)) {
-    map.removeLayer(markerGroup);
-  }
+	if (map.hasLayer(markerGroup)) {
+		map.removeLayer(markerGroup);
+	}
 
-  markerGroup = L.segmentMarkerGroup(options);
+	markerGroup = L.segmentMarkerGroup(options);
 
-  var templesJson = L.geoJSON(temples);
-  var templeLayers = templesJson.getLayers();
+	var templesJson = L.geoJSON(temples);
+	var templeLayers = templesJson.getLayers();
 
-  markerGroup.addLayers(templeLayers);
+	markerGroup.addLayers(templeLayers);
 
-  console.log(markerGroup);
+	console.log(markerGroup);
 
-  markerGroup.addTo(map);
+	markerGroup.addTo(map);
 };
